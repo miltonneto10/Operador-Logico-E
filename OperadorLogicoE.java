95656ddd9a51fa9b6cab5f9f478f7a4678b57346
Milton Salustiano <109237764+miltonneto10@users.noreply.github.com>
2023-08-05 15:24:42 -0300

    Commit Inicial

---
+++ OperadorLogicoE.java
@@ -0,0 +1,18 @@
+/**
+Classe utilizada para demonstrar o uso do operador Lógico E (& &)
+**/
+
+public class OperadorLogicoE {
+	public static void main (String [] args) {
+		boolean a = true;
+		boolean b = false;
+		boolean c = true;
+		
+		System.out.println(a && b);
+		System.out.println(a && c);
+	}
+}
+
+/**
+O operador && é chamado de “operador E”. Ele retornará true se os dois operadores lógicos forem verdadeiros, logo, <operador1> && <operador2>
+**/
